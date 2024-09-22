75043130d67b7713968b9a2f6d914e1cbdaaceb7
허세진 <heosejin222@gmail.com>
2024-09-22 23:24:15 +0900

    코드업 (6080)

--- python_practice.py
+++ python_practice.py
@@ -1,10 +1,8 @@
-a = int(input())
-check = 0
-sum = 0
-while 1:
-    if sum >= a:
-        break
-    check = check + 1
-    sum = sum+check
-        
-print(check)
+a,b=input().split()
+
+a=int(a)
+b=int(b)
+
+for i in range(1, a+1) :
+  for j in range(1, b+1) :
+    print(i, j)
