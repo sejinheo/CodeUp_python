402e5e16d4eb0de1b065bb892f488c489ede520e
허세진 <heosejin222@gmail.com>
2024-09-23 08:06:11 +0900

    코드업 (6082)

--- python_practice.py
+++ python_practice.py
@@ -1,4 +1,9 @@
-a = int(input(), 16)
+a = int(input())
 
-for i in range(1,16):
-    print("%X*%X=%X"%(a,i,a*i))
+for i in range(1, a+1):
+    
+    if (i % 10 == 3 or i % 10 == 6 or i % 10 == 9):
+        print("X", end=' ')
+        
+    else:
+        print(i, end=' ')
