d06a6c610d50edf9f85c228f1f050b2d62998d24
허세진 <heosejin222@gmail.com>
2024-09-24 23:30:49 +0900

    코드업 (6088)

--- python_practice.py
+++ python_practice.py
@@ -1,6 +1,11 @@
-a = input()
-a = int(a)
-for i in range(1, a+1) :
-  if i%3==0:
-    continue            
-  print(i, end=' ')
+a,d,n=input().split()
+
+a=int(a)
+d=int(d)
+n=int(n)
+
+s=a
+for i in range(2, n+1):
+   s+=d
+
+print(s)
