5e0d272fa08e2a66154f48a698a28f70ea597e46
허세진 <heosejin222@gmail.com>
2024-09-25 23:31:01 +0900

    코드업 (6096)

--- python_practice.py
+++ python_practice.py
@@ -4,12 +4,28 @@ for i in range(20) :
   for j in range(20) : 
     d[i].append(0)
 
+for i in range(19) :
+  a = input().split()
+  for j in range(19) :
+    d[i+1][j+1] = int(a[j])
+
 n = int(input())
 for i in range(n) :
-  x, y = input().split()
-  d[int(x)][int(y)] = 1
+  x,y=input().split()
+  x=int(x)
+  y=int(y)
+  for j in range(1, 20) :
+    if d[j][y]==0 :
+      d[j][y]=1
+    else :
+      d[j][y]=0
+
+    if d[x][j]==0 :
+      d[x][j]=1
+    else :
+      d[x][j]=0
 
 for i in range(1, 20) :
-  for j in range(1, 20) : 
+  for j in range(1, 20) :
     print(d[i][j], end=' ')
   print()
