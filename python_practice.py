18727ca80c4d447ff97dcd3d662bb46f7f0bd344
허세진 <heosejin222@gmail.com>
2024-09-25 23:22:25 +0900

    코드업 (6095)

--- python_practice.py
+++ python_practice.py
@@ -1,12 +1,15 @@
+d=[]
+for i in range(20) :
+  d.append([])
+  for j in range(20) : 
+    d[i].append(0)
+
 n = int(input())
-a = input().split()
-
 for i in range(n) :
-  a[i] = int(a[i])
+  x, y = input().split()
+  d[int(x)][int(y)] = 1
 
-min = a[0]
-for i in range(0, n) :
-  if a[i] < min :
-    min = a[i]
-
-print(min)
+for i in range(1, 20) :
+  for j in range(1, 20) : 
+    print(d[i][j], end=' ')
+  print()
